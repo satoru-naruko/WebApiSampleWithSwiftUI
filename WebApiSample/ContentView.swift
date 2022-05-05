b75a145aecf2664de7bac5fbc3f8c9ed757b1058
satoru-naruko <naruko505@gmail.com>
2022-05-05 11:10:50 +0900

    Initial Commit

---
+++ WebApiSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebApiSample
+//
+//  Created by 鳴子智 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebApiSample/WebApiSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebApiSampleApp.swift
+//  WebApiSample
+//
+//  Created by 鳴子智 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct WebApiSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
